b7f00a4f52694e09a649ef5dad0aeadea5f6c1d4
brrimolo <bruno.rimolo@gmail.com>
2021-10-19 22:55:51 -0300

    ...

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { StyleSheet} from 'react-native';
 import { SafeAreaView, View } from 'react-native';
+
 import Login from './src/telas/Login';
+import mock from './src/mocks/login'
 
 export default function App() {
   return (
     <>
       <SafeAreaView>
         <View style={ styles.container }>
-          <Login/>
+          <Login {...mock}/>
         </View>
       </SafeAreaView>
     </>
