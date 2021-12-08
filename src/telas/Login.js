a73bcf5af47e01bff801beb7bf86183bcba34dff
brrimolo <bruno.rimolo@gmail.com>
2021-12-07 21:01:13 -0300

    Mudancas Pagamento / Login

--- src/telas/Login.js
+++ src/telas/Login.js
@@ -1,16 +1,64 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import InputBox from '../components/InputBox';
 import Botao from '../components/Botao';
 
 
-export default function Login() {
+export default function Login({ navigation }) {
   
   const [login, setLogin] = useState('');
   const onChangeLogin = login => setLogin(login);
   const [password, setPassword] = useState('');
   const onChangePassword = password => setPassword(password);
+
+  function efetuarLogin() {
+    var flogin = {
+        "email":this.state.email,
+        "senha":this.state.senha
+    }
+    var requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(flogin)
+    }
+
+    if(this.state.TipoLogin === "funcionario"){
+        var clogin = {
+            "login":this.state.email,
+            "senha":this.state.senha
+        }
+        var requestOption = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(clogin)
+        }
+        var url = window.servidor + '/funcionario/login'
+        fetch(url,requestOption)
+            .then(response => response.json())
+            .then(data => this.setState({clienteLogin: data}) )
+            .then(() => {
+                let nomeUsuario = this.state.clienteLogin.nome;
+                if(this.state.clienteLogin.nome==='null'){
+                    sessionStorage.setItem('NomeLogin','null')
+                    sessionStorage.setItem('IdUsuarioLogado','null')
+                }else{
+                    sessionStorage.setItem('NomeLogin',this.state.clienteLogin.nome)
+                    sessionStorage.setItem('IdUsuarioLogado',this.state.clienteLogin.matricula)
+                    toast.success('Benvindo '+ nomeUsuario)
+                    sessionStorage.setItem('TipoDeLogin','funcionario')
+                    this.setState({redirecionar: true})
+                    window.location.reload()
+
+                }
+                
+            })
+    }
+  }
 
 
   return (
@@ -34,6 +82,12 @@ export default function Login() {
           texto='Logar'
           style={ estilos.botao }
           onPress={ onChangePassword }
+        />
+        <Text>Caso não tenha cadastro clique aqui:</Text>
+        <Botao 
+          texto='Cadastrar'
+          style={ estilos.botao }
+          onPress={ () => navigation.navigate('Cadastro de Usuário') }
         />
       </View>
     </>
--- src/telas/Pagamento.js
+++ src/telas/Pagamento.js
@@ -1,81 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import Botao from '../components/Botao';
-import Clientes from '../components/Clientes';
 import Radio from '../components/Radio';
+import Botao from '../components/Botao';
 
 export default class Pagamento extends React.Component {
 
-
-  state = {
-    clientes: []
-  };
-  
-  getJsonData = () => {
-    const url = window.servidor + '/cliente/'
-    const requestOptions = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    };
-
-    fetch(url, requestOptions)
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson);
-        this.setState({
-          clientes: responseJson
-        })
-      })
-      .catch((error) => {
-        console.error
-      })
-  }
-
-  getAllClientes = () => {
-    const url = window.servidor + "/cliente/"
-    fetch(url)
-      .then(response => response.json())
-      .then((data) => {
-          this.setState({clientes: data})
-          console.log(data)
-      })
-  }
-
-  componentDidMount = () => {
-    this.getJsonData()
-  }
-
-  funcNomeChange = (event) => {
-    this.setState({nome: event.target.value})
-  }
-  funcCpfChange = (event) => {
-    this.setState({cpf: event.target.value})
-  }
-  funcEmailChange = (event) => {
-    this.setState({email: event.target.value})
-  }
-  funcSenhaChange = (event) => {
-    this.setState({senha: event.target.value})
-  }
-  funcClienteSelecionadoChange = (event) => {
-    this.setState({clienteSelecionado: event.target.value})
-  }
-
-  
-
   render() {
-
     return (
       <>
         <View>
-          <Clientes { ...this.state.clientes }/>
-        </View>
-
-        <View>
-          <Botao texto='Adicionar' style={ estilos.botao } onPress={ () => this.getJsonData() } />
           <Radio 
             sel={ 0 }
             options={ ['Cartão','Dinheiro'] }
@@ -83,19 +17,18 @@ export default class Pagamento extends React.Component {
             onChangeSelect={ (opt, index) => alert(index) }
           />
         </View>
+        <View>
+          <Botao texto='Finalizar' style={ estilos.botao } onPress={ () => alert('Seu pedido será enviado !') } />
+        </View>
       </>
     );
-
   }
-
 }
 
 const estilos = StyleSheet.create( {
-
   botao: {
     margin: 30,
   },
-
   tela: {
     flex: 1,
   },
